c3ad879b119a021af21b8b48c4e2c806e7bf6546
Maria Komissarova <shinonhorror@gmail.com>
2022-10-25 23:51:09 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import PostList from "./components/PostList";
 import PostFilter from "./components/PostFilter";
 import MyButton from "./components/UI/button/MyButton";
@@ -8,6 +8,9 @@ import PostForm from "./components/PostForm";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
 import { useFetching } from "./components/hooks/useFetching";
+import { getPagesCount } from "./utils/pages";
+import { usePagination } from "./components/hooks/usePagination";
+
 import "./styles/app.css";
 
 function App() {
@@ -17,15 +20,27 @@ function App() {
     query: "",
   });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchingPost = usePosts(posts, filter.sort, filter.query);
+  const pagesArray = usePagination(totalPages);
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPagesCount(totalCount, limit));
   });
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]); // создание поста (добавление поста в posts)
     setModal(false);
   };
+
+  useEffect(() => {
+    fetchPosts()
+  }, [page]); // в качестве объекта отслеживания пустой массив, чтобы функция вызвалась только один раз
 
   useEffect(() => {
     fetchPosts();
@@ -62,6 +77,16 @@ function App() {
           title="Список постов 1"
         />
       )}
+      <div className="page__wrapper">
+        {pagesArray.map((p) => (
+          <span 
+          onClick={()=>setPage(p)}
+          key={p} 
+          className={page === p ? "page page__current" : "page"}>
+          {p}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
