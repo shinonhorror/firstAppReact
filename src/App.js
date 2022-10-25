c6dc0e82506bd9b93c31e80f6173246c52948a41
Maria Komissarova <shinonhorror@gmail.com>
2022-10-25 22:47:47 +0300

    Add a new hook, new UI component - loader

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import PostList from "./components/PostList";
 import PostFilter from "./components/PostFilter";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./components/hooks/usePosts";
 import MyModal from "./components/UI/MyModal/MyModal";
 import PostForm from "./components/PostForm";
-import axios from 'axios';
+import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
+import { useFetching } from "./components/hooks/useFetching";
 import "./styles/app.css";
 
 function App() {
   const [posts, setPosts] = useState([]);
-
   const [filter, setFilter] = useState({
     sort: "",
     query: "",
   });
-
   const [modal, setModal] = useState(false);
-
   const sortedAndSearchingPost = usePosts(posts, filter.sort, filter.query);
-
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
   const createPost = (newPost) => {
     setPosts([...posts, newPost]); // создание поста (добавление поста в posts)
     setModal(false);
   };
 
-  async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    setPosts(response.data)
-  }
+  useEffect(() => {
+    fetchPosts();
+  }, []); // в качестве объекта отслеживания пустой массив, чтобы функция вызвалась только один раз
+
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id)); // удаление поста по его id
   };
 
   return (
     <div className="App">
-      <button onClick={fetchPosts}>GET POSTS</button>
       <MyButton style={{ marginTop: "15px" }} onClick={() => setModal(true)}>
         Создать пост
       </MyButton>
@@ -43,11 +44,24 @@ function App() {
         <PostForm create={createPost} />
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchingPost}
-        title="Список постов 1"
-      />
+      {postError && <h1>Произошла ошибка</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchingPost}
+          title="Список постов 1"
+        />
+      )}
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Counter = function () {
+const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
