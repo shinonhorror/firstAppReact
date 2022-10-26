ba5c6ea15cedf9c9f183f9c3f93adfbbfa5b709c
Maria Komissarova <shinonhorror@gmail.com>
2022-10-26 18:19:14 +0300

    add a selecting numbers of posts on posts-page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PostList from "../components/PostList";
 import PostFilter from "../components/PostFilter";
 import MyButton from "../components/UI/button/MyButton";
 import { usePosts } from "../components/hooks/usePosts";
 import MyModal from "../components/UI/MyModal/MyModal";
+import { useObserver } from "../components/hooks/useObserver";
+import MySelect from "./../components/UI/select/MySelect";
 import PostForm from "../components/PostForm";
 import PostService from "../API/PostService";
 import Pagination from "../components/UI/pagination/Pagination";
@@ -24,10 +26,12 @@ function Posts() {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const sortedAndSearchingPost = usePosts(posts, filter.sort, filter.query);
+  const lastElement = useRef();
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    // setPosts([...posts, ...response.data]);
+    setPosts(response.data)
     const totalCount = response.headers["x-total-count"];
     setTotalPages(getPagesCount(totalCount, limit));
   });
@@ -37,13 +41,12 @@ function Posts() {
     setModal(false);
   };
 
+  // useObserver(lastElement, page < totalPages, isPostsLoading, () =>
+  //   setPage(page + 1)
+  // );
   useEffect(() => {
     fetchPosts();
-  }, [page]); // в качестве объекта отслеживания пустой массив, чтобы функция вызвалась только один раз
-
-  useEffect(() => {
-    fetchPosts();
-  }, []); // в качестве объекта отслеживания пустой массив, чтобы функция вызвалась только один раз
+  }, [page, limit]); // в качестве объекта отслеживания пустой массив, чтобы функция вызвалась только один раз
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id)); // удаление поста по его id
@@ -62,8 +65,26 @@ function Posts() {
         <PostForm create={createPost} />
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter} />
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(value)}
+        defaultValue="Количество элементов на странице"
+        options={
+          [{value: 5, name: "5"},
+          {value: 10, name: "10"},
+          {value: 25, name: "25"},
+          {value: -1, name: "Показать все посты"},
+        ]
+        }
+      />
       {postError && <h1>Произошла ошибка</h1>}
-      {isPostsLoading ? (
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchingPost}
+        title="Тестовые посты"
+      />
+      {/* <div ref={lastElement} style={{ height: "20px" }}></div> */}
+      {isPostsLoading && (
         <div
           style={{
             display: "flex",
@@ -73,12 +94,6 @@ function Posts() {
         >
           <Loader />
         </div>
-      ) : (
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchingPost}
-          title="Список постов 1"
-        />
       )}
       <Pagination page={page} totalPages={totalPages} changePage={changePage} />
     </div>
